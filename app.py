c8fecbea080ec8e66a554b8e894196e0b782325e
Gee Cheung <gcheung16@hotmail.com>
2024-06-27 00:22:35 +0100

    feat: i have a simple python api

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# In-memory storage for simplicity
+posts = [
+    {'id': 1, 'title': 'First Post', 'body': 'This is the first post', 'userId': 1},
+    {'id': 2, 'title': 'Second Post', 'body': 'This is the second post', 'userId': 1}
+]
+
+# Get all posts
+@app.route('/posts', methods=['GET'])
+def get_posts():
+    return jsonify(posts)
+
+# Get a single post by ID
+@app.route('/posts/<int:post_id>', methods=['GET'])
+def get_post(post_id):
+    post = next((post for post in posts if post['id'] == post_id), None)
+    if post:
+        return jsonify(post)
+    return jsonify({'error': 'Post not found'}), 404
+
+# Create a new post
+@app.route('/posts', methods=['POST'])
+def create_post():
+    new_post = request.get_json()
+    new_post['id'] = len(posts) + 1
+    posts.append(new_post)
+    return jsonify(new_post), 201
+
+# Update an existing post
+@app.route('/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
+    updated_post = request.get_json()
+    post = next((post for post in posts if post['id'] == post_id), None)
+    if post:
+        post.update(updated_post)
+        return jsonify(post)
+    return jsonify({'error': 'Post not found'}), 404
+
+# Delete a post
+@app.route('/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    global posts
+    posts = [post for post in posts if post['id'] != post_id]
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
